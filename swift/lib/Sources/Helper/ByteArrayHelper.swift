1f9fee0d749d343d801832b8821a49af8b320a97
Paulo Coutinho <paulocoutinhox@gmail.com>
2023-02-01 15:57:04 -0300

    create dataview class to transfer pointer data

--- swift/lib/Sources/Helper/ByteArrayHelper.swift
+++ swift/lib/Sources/Helper/ByteArrayHelper.swift
@@ -17,4 +17,10 @@ public class ByteArrayHelper {
         let bufferPointer = bytePointer.bindMemory(to: UInt8.self, capacity: size)
         return Array(UnsafeBufferPointer(start: bufferPointer, count: size))
     }
+
+    public static func createFromDataView(_ dataView: DataView) -> [UInt8] {
+        let bytePointer = UnsafeMutableRawPointer(bitPattern: dataView.ptr)!
+        let bufferPointer = bytePointer.bindMemory(to: UInt8.self, capacity: Int(dataView.size))
+        return Array(UnsafeBufferPointer(start: bufferPointer, count: Int(dataView.size)))
+    }
 }
